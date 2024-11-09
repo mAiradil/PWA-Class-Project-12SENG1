f8350d74b19ef0c8de10f924caac6f9ba4c9d3b0
mAira <maira.binteadil@education.nsw.gov.au>
2024-11-09 19:22:42 +1100

    Added a Javascript and added CSS code and  added a search button
    I added a search bar for my html website, this will be functional later on, for the index.js, i have made it so when you touch the search bar and type whatever, the 'x' mark wull make it disappear, and i Just styled it.

---
+++ myPWA/frontend/index.js
@@ -0,0 +1,21 @@
+const searchon = document.querySelector('#searchopen');
+const searchoff = document.querySelector('#removesearch');
+const searchinput = document.querySelector('.searchinput');
+
+searchinput.style.display = "none";
+
+searchon.addEventListener('click', () => {
+    if (searchinput.style.display === 'none') {
+        searchinput.style.display = 'flex';
+    } else {
+        searchinput.style.display = 'none';
+    }
+});
+
+searchoff.addEventListener('click', () => {
+    if (searchinput.style.display === 'flex') {
+        searchinput.style.display = 'none';
+    } else {
+        searchinput.style.display = 'flex';
+    }
+});
